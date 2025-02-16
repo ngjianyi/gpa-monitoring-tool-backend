e4cbbfede7241b16d18b13e867ccb6c124524a85
ngjianyi <ngjianyi2002@gmail.com>
2025-02-16 23:34:54 +0800

    Add support for heroku deployment

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import os
+
 from models.models import db
 from routes.student import bp
+
 def create_app():
     app = Flask(__name__)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' 
+
+    database_url = os.environ.get('DATABASE_URL')
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+        os.environ['DATABASE_URL'] = database_url
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
